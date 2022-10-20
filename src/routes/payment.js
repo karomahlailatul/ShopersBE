1627fdbec14c538f32aae1c87674fe9021d569cc
Lailatul Karomah <karomahlailatul@gmail.com>
2022-10-20 14:20:53 +0700

    bug deployment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controller/Payment');
+const paymentController = require('../controller/payment');
 const { protect } = require('../middlewares/auth')
 const {
     checkInputPayment,
